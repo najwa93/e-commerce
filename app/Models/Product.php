f641bab20317c10a53cb2be29660e0a99f629551
najwa93 <43004016+najwa93@users.noreply.github.com>
2020-04-04 22:51:27 +0300

    add project models and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status', //available or not available
+        'image',
+        'seller_id'];
+
+    public function isAvailable(){
+        return $this->status = Product::AVAILABLE_PRODUCT;
+    }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+}
---
+++ app/Models/Seller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Seller extends User
+{
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+}
